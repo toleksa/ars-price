e17a11b5756d9f6bbc5a50c73f52acaecc0e772b
root <root@centos1.ac>
2021-07-21 22:08:28 +0200

    selinux context

--- generate.sh
+++ generate.sh
@@ -14,7 +14,5 @@ if [ "$RC" -ne 0 ]; then
     exit 1
 fi
 
-cat price-log.txt | gawk '{ print $1 }' \
-    | gnuplot -p -e 'set style line 1 lt 1 lw 3 pt 3 linecolor rgb "blue" ; set title "ARS SteamCode price" ; set ylabel "EuroCents" ; set term svg; set output "|rsvg-convert -f png -o out.png /dev/stdin"; plot "/dev/stdin" with lines' \
-    && mv out.png /var/www/html/ 
+cat price-log.txt | gawk '{ print $1 }' | gnuplot -p -e 'set style line 1 lt 1 lw 3 pt 3 linecolor rgb "blue" ; set title "ARS SteamCode price" ; set ylabel "EuroCents" ; set term svg; set output "|rsvg-convert -f png -o out.png /dev/stdin"; plot "/dev/stdin" with lines' && chcon -t httpd_sys_content_t out.png && mv out.png /var/www/html/ 
 
