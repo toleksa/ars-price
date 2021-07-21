a4ed8722ba2a059da8554b20b20553f7d965ee7f
root <root@centos1.ac>
2021-07-21 22:03:06 +0200

    check if 'rsvg-convert' is installed

--- generate.sh
+++ generate.sh
@@ -7,6 +7,13 @@ if [ "$RC" -ne 0 ]; then
     exit 1
 fi
 
+# check if 'rsvg-convert' is installed
+which rsvg-convert > /dev/null 2>&1; RC=$?
+if [ "$RC" -ne 0 ]; then
+    echo "ERR: no 'rsvg-convert' found in \$PATH"
+    exit 1
+fi
+
 cat price-log.txt | gawk '{ print $1 }' \
     | gnuplot -p -e 'set style line 1 lt 1 lw 3 pt 3 linecolor rgb "blue" ; set title "ARS SteamCode price" ; set ylabel "EuroCents" ; set term svg; set output "|rsvg-convert -f png -o out.png /dev/stdin"; plot "/dev/stdin" with lines' \
     && mv out.png /var/www/html/ 
