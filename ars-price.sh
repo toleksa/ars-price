b26d9b19ef090304d534e850f219f5f70d8339cf
toleksa <toleksa@gmail.com>
2021-07-21 20:16:16 +0200

    cleanup for better readibility

--- ars-price.sh
+++ ars-price.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
-FILE=$HOME/ars-price/ars-price.txt
+SITE="https://www.eneba.com/steam-steam-wallet-gift-card-1000-ars-key-argentina"
 
-PRICE=$(curl --silent https://www.eneba.com/steam-steam-wallet-gift-card-1000-ars-key-argentina | grep 'itemProp="price"' | sed -e 's/meta itemProp="price" content="/\n/g' | gawk -F"\"" '{ print $1 }' | grep -Ev '^$' | head -n 2 | tail -n 1 )
+PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+FILE_BEST_PRICE="$PWD/ars-price.txt"
+FILE_PRICE_LOG="$PWD/ars-price.txt.log"
+ALERT_CMD="$HOME/alert.sh"
+
+which bc > /dev/null 2>&1; RC=$?
+if [ "$RC" -ne 0 ]; then
+    echo "ERR: no 'bc' found in \$PATH"
+    exit 1
+fi
+
+# get price
+#PRICE=$(curl --silent "$SITE" | grep 'itemProp="price"' | sed -e 's/meta itemProp="price" content="/\n/g' | gawk -F"\"" '{ print $1 }' | grep -Ev '^$' | head -n 2 | tail -n 1 )
+PRICE=$(curl --silent "$SITE" \
+        | grep 'itemProp="price"' \
+        | gawk -F"meta itemProp=\"price\" content=\"" ' { print $2 }' \
+        | gawk -F"\"" '{ print $1 }')
 
 PRICE=`echo "($PRICE*100)/1" | bc`
 
-echo "$PRICE `date +%Y%m%d-%H%M`">> $FILE.log
+echo "$PRICE `date +%Y%m%d-%H%M`">> $FILE_PRICE_LOG
 
-if [ ! -f "$FILE" ]; then
-    echo "$PRICE `date +%Y%m%d-%H%M`"> $FILE
+if [ ! -f "$FILE_BEST_PRICE" ]; then
+    echo "$PRICE `date +%Y%m%d-%H%M`"> $FILE_BEST_PRICE
     exit 0
 fi
 
-BEST_PRICE=$(tail -n 1 $FILE | gawk '{ print $1 }')
+BEST_PRICE=$(tail -n 1 "$FILE_BEST_PRICE" | gawk '{ print $1 }')
 
-if [ $PRICE -lt $BEST_PRICE ]; then
-    /root/alert.sh "better ars price: $PRICE"
-    echo "$PRICE `date +%Y%m%d-%H%M`"> $FILE
+if [ "$PRICE" -lt "$BEST_PRICE" ]; then
+    "$ALERT_CMD" "better ars price: $PRICE"
+    echo "$PRICE `date +%Y%m%d-%H%M`"> $FILE_BEST_PRICE
 fi
 
 
