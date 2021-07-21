bba8443405bbb77a7c0ae936e16740544ada37b5
root <root@centos1.ac>
2021-07-21 22:01:26 +0200

    check if gnuplot is installed

--- generate.sh
+++ generate.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-cat price-log.txt | gawk '{ print $1 }' | gnuplot -p -e 'set style line 1 lt 1 lw 3 pt 3 linecolor rgb "blue" ; set title "ARS SteamCode price" ; set ylabel "EuroCents" ; set term svg; set output "|rsvg-convert -f png -o out.png /dev/stdin"; plot "/dev/stdin" with lines' && mv out.png /var/www/html/ 
+# check if 'gnuplot' is installed
+which gnuplot > /dev/null 2>&1; RC=$?
+if [ "$RC" -ne 0 ]; then
+    echo "ERR: no 'gnuplot' found in \$PATH"
+    exit 1
+fi
+
+cat price-log.txt | gawk '{ print $1 }' \
+    | gnuplot -p -e 'set style line 1 lt 1 lw 3 pt 3 linecolor rgb "blue" ; set title "ARS SteamCode price" ; set ylabel "EuroCents" ; set term svg; set output "|rsvg-convert -f png -o out.png /dev/stdin"; plot "/dev/stdin" with lines' \
+    && mv out.png /var/www/html/ 
 
