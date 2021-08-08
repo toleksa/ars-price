5d642fa071c8652f1faf20e0833ad4a1b593a9d3
toleksa <toleksa@gmail.com>
2021-08-08 16:18:15 +0200

    price reading fix

--- ars-price.sh
+++ ars-price.sh
@@ -16,18 +16,28 @@ if [ "$RC" -ne 0 ]; then
     exit 1
 fi
 
-# get price
-#PRICE=$(curl --silent "$SITE" | grep 'itemProp="price"' | sed -e 's/meta itemProp="price" content="/\n/g' | gawk -F"\"" '{ print $1 }' | grep -Ev '^$' | head -n 2 | tail -n 1 )
-PRICE=$(curl --silent "$SITE" \
-        | grep 'itemProp="price"' \
-        | gawk -F"meta itemProp=\"price\" content=\"" ' { print $2 }' \
-        | gawk -F"\"" '{ print $1 }')
+# get price #20210808 - second method seems to generate two numbers, failback to first method
+PRICE=$(curl --silent "$SITE" | grep 'itemProp="price"' | sed -e 's/meta itemProp="price" content="/\n/g' | gawk -F"\"" '{ print $1 }' | grep -Ev '^$' | head -n 2 | tail -n 1 )
+#PRICE=$(curl --silent "$SITE" \
+#        | grep 'itemProp="price"' \
+#        | gawk -F"meta itemProp=\"price\" content=\"" ' { print $2 }' \
+#        | gawk -F"\"" '{ print $1 }')
+
+# change empty to 0, because empty/no value breaks chart generator
+if [ "$PRICE" == '' ]; then
+    PRICE=0
+fi 
 
 # convert Euro to Cents
 PRICE=`echo "($PRICE*100)/1" | bc`
 
 # write current PRICE to log
 echo "$PRICE `date +%Y%m%d-%H%M`">> $FILE_PRICE_LOG
+
+# 0 means bad reading, so don't update best-price
+if [ "$PRICE" -eq 0 ]; then
+    exit 0
+fi
 
 # if there is no FILE_BEST_PRICE, create with current PRICE and exit
 if [ ! -f "$FILE_BEST_PRICE" ]; then
