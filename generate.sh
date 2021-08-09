6862240f883663d29f5cb5347a0f074f0ab12d62
toleksa <toleksa@gmail.com>
2021-08-09 04:26:35 +0200

    proper timeline, don't draw 0 values, overall styling

--- generate.sh
+++ generate.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 ARG=""
-ARG="$ARG set style line 1 lt 1 lw 3 pt 3 linecolor rgb 'blue' ;"
-ARG="$ARG set title 'ARS SteamCode price - `date +%Y%m%d-%H:%M:%S`' ;"
+ARG="$ARG set title 'ARS SteamCode price - generated `date '+%Y-%m-%d %H:%M:%S'`' ;"
 ARG="$ARG set ylabel 'EuroCents' ;"
+#ARG="$ARG set xlabel 'Timeline' ;"
+ARG="$ARG set xdata time ;"
+ARG="$ARG set timefmt '%Y%m%d-%H%M';"
+ARG="$ARG set format x '%Y-%m-%d';"
+ARG="$ARG set autoscale ;"
+#ARG="$ARG set yrange [500:] ;"
+ARG="$ARG set datafile sep ' ';"
+ARG="$ARG set key top right autotitle columnheader ;"
+ARG="$ARG set grid;"
 ARG="$ARG set term png size 1200,800;"
-ARG="$ARG plot '/dev/stdin' with lines"
-
-#TODO: try to not draw 0 values
+ARG="$ARG plot '/dev/stdin' using 1:2 with lines lt 1 lw 2 linecolor rgb 'blue' title 'Price in Eurocents'"
 
 # using https://hub.docker.com/r/jess/gnuplot
-gawk '{ print $1 }' price-log.txt | docker run -i --name gnuplot jess/gnuplot -p -e "$ARG" > /www/files/out.png
+gawk '{ if ($1 == 0) next; print $2" "$1 }' price-log.txt | docker run -i --name gnuplot jess/gnuplot -p -e "$ARG" > /www/files/out.png
 docker rm gnuplot
 
